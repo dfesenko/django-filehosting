db08bc43ac3a5a9f5f62383159a4e2d16c6fc2be
dfesenko5 <fesenkogithub@gmail.com>
2020-07-23 09:09:55 +0300

    Refactor: remove print() from the Celery task

--- filehosting/core/tasks.py
+++ filehosting/core/tasks.py
@@ -7,4 +7,3 @@ from .models import FileObject
 @app.task
 def remove_expired_files():
     expired_files = FileObject.objects.filter(expiration_at__lt=timezone.now()).delete()
-    print(expired_files)
