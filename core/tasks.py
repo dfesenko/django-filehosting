a73e6998dee4ddad00bb148ee724e3e74380f1f8
dfesenko5 <fesenkogithub@gmail.com>
2020-07-24 10:29:47 +0300

    Fix: Celery task deletes file object from DB, but not from the filesystem.
    The reason was the wrong path to the file in FS in tasks.py.

--- core/tasks.py
+++ core/tasks.py
@@ -3,7 +3,17 @@ from django.utils import timezone
 from filehosting.celery import app
 from .models import FileObject
 
+import shutil
+import os
+
 
 @app.task
 def remove_expired_files():
-    expired_files = FileObject.objects.filter(expiration_at__lt=timezone.now()).delete()
+    expired_files = FileObject.objects.filter(expiration_at__lt=timezone.now())
+
+    for file in expired_files:
+        path_to_file = os.path.join(os.getcwd(), str(file))
+        path_to_file_directory = "/".join(path_to_file.split("/")[:-1])
+        shutil.rmtree(path_to_file_directory, ignore_errors=True)
+
+    expired_files.delete()
